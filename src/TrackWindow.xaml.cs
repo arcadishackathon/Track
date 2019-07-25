30d26df969794189d42c55da60c3356a8b1698dc
Michael <Michaelvantelgen@gmail.com>
2019-07-25 09:26:04 -0400

    Initial commit

---
+++ src/TrackWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Track
+{
+    /// <summary>
+    /// Interaction logic for TrackWindow.xaml
+    /// </summary>
+    public partial class TrackWindow : Window
+    {
+        public TrackWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
